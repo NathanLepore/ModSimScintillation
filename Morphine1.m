99516a86c6e98be581df62b5937164ec40ad40de
NathanLepore <nathaniel.lepore@students.olin.edu>
2016-09-27 17:30:52 -0400

    Newest Version with half lives

--- Morphine1.m
+++ Morphine1.m
@@ -1,38 +1,39 @@
 clf
-molecularweight  = 285.33; % molecular weight of morphine
-LitersOfPlasma = 5; % liters of plasma in the body
-Morphine = 0; % initial morphine concentration
-Injection = 1.25; % milligrams of morphine
-Intake = Injection/molecularweight/LitersOfPlasma; % intake of morphine
-M3G = 0; % initial m3g concentration
-M6G = 0; % initial m6g concentration
-a = 12; % length of simulation
+molecularweight  = 285.33;
+LitersOfPlasma = 5;
+Morphine = 0;
+Injection = 1.25;
+Intake = Injection/(molecularweight*LitersOfPlasma);
+M3G = 0;
+M6G = 0;
+a = 12;
 hold on
-% initialize datasets
 M6 = [1:a];
 M3 = [1:a];
 MM = [1:a];
-q = [1:a]; % time
-for i = 1:12
+q = [1:a];
+
+for i = 1:a
 if i < 5
-    MorphineX = (Intake); %this is the initial intake of morphine for the first 5 iterations
+    MorphineX = (Intake);
     
 else MorphineX = 0;
     
 end
     MM(i) = Morphine;
+    loglog(i,Morphine,'b.')
     M3G1 = (Morphine*.6);
-    M6G1 = (Morphine*.1);
-    M3G = M3G*.2 + M3G1;
-    M6G = M6G*.2 + M6G1;
+    M6G1 = (Morphine*.2);
+    M3G = (M3G + M3G1)*((2).^(-i/4.1));
+    M6G = (M6G + M6G1)*((2).^(-i/2.8));
     M3(i) = M3G;
     M6(i) = M6G;
-    Morphine = (MorphineX + Morphine - (M3G + M6G));
+    Morphine = (MorphineX + Morphine - (M3G1 + M6G1));
     
     
-    %plot(i,Morphine,'b.')
-    %plot(i,M3G,'r.')
-    %plot(i,M6G,'g.')
+   
+    loglog(i,M3G,'r.')
+    loglog(i,M6G,'g.')
 end
 loglog(q,M3,'r')
 loglog(q,M6,'g')
