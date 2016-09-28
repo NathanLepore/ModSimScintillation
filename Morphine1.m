c167cb88397683d3d0e511559077fd347eb59822
NathanLepore <nathaniel.lepore@students.olin.edu>
2016-09-28 16:35:27 -0400

    Close enough if you manually change the y-axis to a log scale

--- Morphine1.m
+++ Morphine1.m
@@ -3,7 +3,7 @@ molecularweight  = 285.33;
 LitersOfPlasma = 5;
 Morphine = 0;
 Injection = 1.25;
-Intake = Injection/(molecularweight*LitersOfPlasma);
+Intake = Injection/(molecularweight*LitersOfPlasma)*1000000000;
 M3G = 0;
 M6G = 0;
 a = 12;
@@ -12,28 +12,29 @@ M6 = [1:a];
 M3 = [1:a];
 MM = [1:a];
 q = [1:a];
-
-for i = 1:a
+M3(1) = .001;
+M6(1) = .001;
+MM(1) = .001;
+for i = 2:a
 if i < 5
-    MorphineX = (Intake);
-    
-else MorphineX = 0;
-    
+    MorphineX = (Intake)*((2).^(-i/.28));  
+
 end
-    MM(i) = Morphine;
-    loglog(i,Morphine,'b.')
+    
+    Morphine = (MorphineX + Morphine - (M3G1 + M6G1))*((2).^(-i/2));
+%     plot(i,Morphine,'b.')
     M3G1 = (Morphine*.6);
-    M6G1 = (Morphine*.2);
+    M6G1 = (Morphine*.1);
     M3G = (M3G + M3G1)*((2).^(-i/4.1));
     M6G = (M6G + M6G1)*((2).^(-i/2.8));
     M3(i) = M3G;
     M6(i) = M6G;
-    Morphine = (MorphineX + Morphine - (M3G1 + M6G1));
+    MM(i) = Morphine;
     
     
    
-    loglog(i,M3G,'r.')
-    loglog(i,M6G,'g.')
+%     plot(i,M3G,'r.')
+%     plot(i,M6G,'g.')
 end
 loglog(q,M3,'r')
 loglog(q,M6,'g')
